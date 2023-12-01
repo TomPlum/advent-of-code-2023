7d88f30fc131825cf368c9392340a47246e3dc4b
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-12-01 08:48:58 +0000

    Day 1 | Kotlin-ifcation of the word and digit calibration strategy

--- implementation/src/main/kotlin/io/github/tomplum/aoc/weather/strategy/WordAndDigitCalibration.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/weather/strategy/WordAndDigitCalibration.kt
@@ -1,46 +1,40 @@
 package io.github.tomplum.aoc.weather.strategy
 
+data class CalibrationValue(val index: Int, val value: Any)
+
 class WordAndDigitCalibration : CalibrationStrategy {
-    private val validNumbers = mapOf(
-        "one" to 1,
-        "two" to 2,
-        "three" to 3,
-        "four" to 4,
-        "five" to 5,
-        "six" to 6,
-        "seven" to 7,
-        "eight" to 8,
-        "nine" to 9
+    private val words = mapOf(
+        "one" to 1, "two" to 2, "three" to 3, "four" to 4,
+        "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9
     )
 
-    override fun calibrate(document: List<String>): Int = document.map { calibrationValue ->
-        val firstDigit = calibrationValue
+    override fun calibrate(document: List<String>): Int = document.sumOf { calibrationString ->
+        val firstDigit = calibrationString
             .firstOrNull { value -> value.isDigit() }
-            ?.let { value -> value.toString() to calibrationValue.indexOf(value) }
+            ?.let { value -> CalibrationValue(calibrationString.indexOf(value), value) }
 
-        val firstWord = validNumbers.keys.mapNotNull { word ->
-            if (calibrationValue.contains(word)) {
-                return@mapNotNull validNumbers[word]!!.toString() to calibrationValue.indexOf(word)
-            }
+        val firstWord = words.keys
+            .filter { word -> calibrationString.contains(word) }
+            .map { word -> CalibrationValue(calibrationString.indexOf(word), words[word]!!) }
+            .minByOrNull { calibrationValue -> calibrationValue.index }
 
-            null
-        }.minByOrNull { (_, index) -> index }
-
-        val lastDigit = calibrationValue
+        val lastDigit = calibrationString
             .lastOrNull { value -> value.isDigit() }
-            ?.let { value -> value.toString() to calibrationValue.lastIndexOf(value) }
+            ?.let { value -> CalibrationValue(calibrationString.lastIndexOf(value), value) }
 
-        val lastWord = validNumbers.keys.mapNotNull { key ->
-            if (calibrationValue.contains(key)) {
-                return@mapNotNull validNumbers[key]!!.toString() to calibrationValue.lastIndexOf(key)
-            }
+        val lastWord = words.keys
+            .filter { word -> calibrationString.contains(word) }
+            .map { word -> CalibrationValue(calibrationString.lastIndexOf(word), words[word]!!) }
+            .maxByOrNull { calibrationValue -> calibrationValue.index }
 
-            null
-        }.maxByOrNull { (_, index) -> index }
+        val first = listOf(firstDigit, firstWord)
+            .minByOrNull { calibrationValue -> calibrationValue?.index ?: Int.MAX_VALUE }!!
+            .value
 
-        val first = listOf(firstDigit, firstWord).minBy { it?.second ?: Int.MAX_VALUE }!!.first
-        val last = listOf(lastDigit, lastWord).maxBy { it?.second ?: Int.MIN_VALUE }!!.first
+        val last = listOf(lastDigit, lastWord)
+            .maxByOrNull { calibrationValue -> calibrationValue?.index ?: Int.MIN_VALUE }!!
+            .value
 
         "$first$last".toInt()
-    }.sum()
+    }
 }
