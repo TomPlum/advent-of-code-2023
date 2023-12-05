2cd029d9e78d4012078d4cd1e8b873660f2682d7
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-12-05 17:27:07 +0000

    Day 5 | Minor improvement to part one solution

--- implementation/src/main/kotlin/io/github/tomplum/aoc/Almanac.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/Almanac.kt
@@ -1,7 +1,5 @@
 package io.github.tomplum.aoc
 
-data class Report(val seed: Int, val values: Pair<String, Int>)
-
 class Almanac(private val data: List<String>) {
     fun findLowestLocationNumber(): Long {
         val seedTypes = data.first().removePrefix("seeds: ").trim().split(" ").map { value -> value.toLong() }
@@ -31,7 +29,7 @@ class Almanac(private val data: List<String>) {
         val seedMap = mutableMapOf<Long, MutableMap<String, Long>>()
 
         categoryMaps.forEach { categoryMap ->
-            val currentMap = mutableMapOf<Pair<Long, Long>, Long>()
+            val currentMap = mutableMapOf<Long, Long>()
 
             seedTypes.forEach { seedNumber ->
                 val sourceCategory = categoryMap.first().sourceCategory
@@ -42,31 +40,18 @@ class Almanac(private val data: List<String>) {
                 }.filterNot { it == null }
 
                 val number: Long = if (candidates.isEmpty()) mappedValue else candidates.first()!!
-                currentMap[Pair(seedNumber, mappedValue)] = number
+                currentMap[seedNumber] = number
             }
 
-            currentMap.forEach { (seedNumbers, mapping) ->
-                val (initialSeedNumber, mappedValue) = seedNumbers
-                if (seedMap.containsKey(initialSeedNumber)) {
-                    val innerMap = seedMap[initialSeedNumber]
+            currentMap.forEach { (seedNumber, mapping) ->
+                if (seedMap.containsKey(seedNumber)) {
+                    val innerMap = seedMap[seedNumber]
                     innerMap?.put(categoryMap.first().targetCategory, mapping)
                 } else {
-                    seedMap[initialSeedNumber] = mutableMapOf(Pair(categoryMap.first().targetCategory, mapping))
+                    seedMap[seedNumber] = mutableMapOf(Pair(categoryMap.first().targetCategory, mapping))
                 }
             }
-            val s = 0
         }
-
-        /*val result = seedTypes.map { seedNumber ->
-            categoryMaps.map { categoryList ->
-                val candidates = categoryList.map { map ->
-                    map.mapSeedNumber(seedNumber)
-                }.filterNot { it == null }
-
-                val number: Long = if (candidates.isEmpty()) seedNumber else candidates.first()!!
-                Report(seedNumber, Pair(categoryList.first().category, number))
-            }
-        }*/
 
         return seedMap.map { it.value }.minOf { it["location"] ?: 0 }
     }
