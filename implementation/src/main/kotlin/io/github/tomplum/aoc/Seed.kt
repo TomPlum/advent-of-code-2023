dde1d6fac109118e9a15740838507d43d84d3c85
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-12-05 17:32:51 +0000

    Day 5 | Major performance improvement to part one solution (176s -> 3ms)

--- implementation/src/main/kotlin/io/github/tomplum/aoc/Seed.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/Seed.kt
@@ -1,25 +1,14 @@
 package io.github.tomplum.aoc
 
 data class SeedAttributeMap(val category: String, val destinationRangeStart: Long, val sourceRangeStart: Long, val rangeLength: Long) {
-    private val sourceRange = sourceRangeStart..<(sourceRangeStart + rangeLength)
-    private val destinationRange = destinationRangeStart..<(destinationRangeStart + rangeLength)
 
     private val categoryParts = category.split(" ")[0].split("-to-")
     val sourceCategory = categoryParts.first()
     val targetCategory = categoryParts.last()
 
-    fun getSourceRange(): LongRange {
-        return sourceRangeStart..<(sourceRangeStart + rangeLength)
-    }
-
-    fun getDestinationRange(): LongRange {
-        return destinationRangeStart..<(destinationRangeStart + rangeLength)
-    }
-
     fun mapSeedNumber(seedNumber: Long): Long? {
-        if (seedNumber in sourceRange) {
-            val index = sourceRange.indexOf(seedNumber)
-            return destinationRange.elementAt(index)
+        if (seedNumber >= sourceRangeStart && seedNumber < (sourceRangeStart + rangeLength)) {
+            return destinationRangeStart + (seedNumber - sourceRangeStart)
         }
 
         return null
