59ab5271e5972974ee2dd7115b7612879924e82a
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-12-08 16:44:08 +0000

    Day 8 | Minor naming/formatting improvements to EtherealNavigation strategy

--- implementation/src/main/kotlin/io/github/tomplum/aoc/network/strategy/EtherealNavigation.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/network/strategy/EtherealNavigation.kt
@@ -5,24 +5,26 @@ import io.github.tomplum.libs.extensions.lcm
 class EtherealNavigation(documents: List<String>) : NetworkNavigationStrategy(documents) {
     override fun navigate(): Long {
         val startingNodes = nodes.keys.filter { node -> node.last() == 'A' }
-
         val currentPosition = startingNodes.associateWith { node -> node }.toMutableMap()
         val stepsTaken = startingNodes.associateWith { _ -> 0L }.toMutableMap()
 
         val endingSnapshots = startingNodes.associateWith { _ -> 0L }.toMutableMap()
 
-        while (endingSnapshots.values.any { it == 0L }) {
+        while (endingSnapshots.values.any { stepsToEnd -> stepsToEnd == 0L }) {
             directions.forEach { direction ->
                 currentPosition.keys.forEach { node ->
-                    if (currentPosition[node]!!.last() == 'Z') {
+                    val currentNodePosition = currentPosition[node]!!
+
+                    if (currentNodePosition.last() == 'Z') {
                         endingSnapshots[node] = stepsTaken[node]!!
                     }
 
                     currentPosition[node] = if (direction == 'R') {
-                        nodes[currentPosition[node]]!!.second
+                        nodes[currentNodePosition]!!.second
                     } else {
-                        nodes[currentPosition[node]]!!.first
+                        nodes[currentNodePosition]!!.first
                     }
+
                     stepsTaken[node] = (stepsTaken[node] ?: 0) + 1
                 }
             }
