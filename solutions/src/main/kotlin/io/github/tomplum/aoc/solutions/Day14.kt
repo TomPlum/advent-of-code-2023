6e0f071860efd7284488876f808dfdc6e8f62a00
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-12-14 09:04:52 +0000

    Day 14 | Add solution part one

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day14.kt
@@ -0,0 +1,15 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.map.platform.PlatformMap
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day14 : Solution<Int, Int> {
+    private val data = InputReader.read<String>(Day(14)).value
+    private val platformMap = PlatformMap(data)
+
+    override fun part1(): Int {
+        return platformMap.calculateNorthSupportBeamsTotalLoad()
+    }
+}
---
+++ solutions/src/test/kotlin/io/github/tomplum/aoc/solutions/Day14Test.kt
@@ -0,0 +1,14 @@
+package io.github.tomplum.aoc.solutions
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class Day14Test {
+    private val solution = Day14()
+
+    @Test
+    fun partOne() {
+        assertThat(solution.part1()).isEqualTo(110274)
+    }
+}
