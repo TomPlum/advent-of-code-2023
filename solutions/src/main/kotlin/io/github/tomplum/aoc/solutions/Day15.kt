61ac0f60978d12a10b37c1c71107104e8927f40f
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-12-15 10:35:57 +0000

    Day 15 | Added solution part one

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day15.kt
@@ -0,0 +1,15 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.hash.HashAlgorithm
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day15: Solution<Int, Int> {
+    private val input = InputReader.read<String>(Day(15)).asSingleString()
+    private val algorithm = HashAlgorithm(input)
+
+    override fun part1(): Int {
+        return algorithm.run()
+    }
+}
